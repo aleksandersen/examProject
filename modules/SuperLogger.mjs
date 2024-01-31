4a088b4b4598e5680ffbcfc128121cc8eaf970df
André Aleksandersen <andrea@uia.no>
2024-01-31 11:41:10 +0100

    Implemented all changes from lecture on Monday 29.1

--- modules/SuperLogger.mjs
+++ modules/SuperLogger.mjs
@@ -1,48 +1,111 @@
-/*
-We need a logging tool that can automate most logging needs. It should be globally avaliable, but also support multiple instances.
-*/
-import chalk from "chalk";
-import {HttpCodes, HTTPMethods} from "./httpConstants.mjs";
 
-let currentThresholdValue = LOGGING_LEVELS.notImportant;
+import Chalk from "chalk";
+import { HTTPMethods } from "./httpConstants.mjs"
 
-let colors = {};
-colors[HTTPMethods.POST] = chalk.yellow;
-colors[HTTPMethods.GET] = chalk.green;
+//#region  Construct for decorating output.
 
+let COLORS = {}; // Creating a lookup tbl to avoid having to use if/else if or switch. 
+COLORS[HTTPMethods.POST] = Chalk.yellow;
+COLORS[HTTPMethods.PATCH] = Chalk.yellow;
+COLORS[HTTPMethods.PUT] = Chalk.yellow;
+COLORS[HTTPMethods.GET] = Chalk.green;
+COLORS[HTTPMethods.DELETE] = Chalk.red;
+COLORS.Default = Chalk.gray;
+
+// Convenience function
+// https://en.wikipedia.org/wiki/Convenience_function
 const colorize = (method) => {
-    return colors[method](method);
+    if (method in COLORS) {
+        return COLORS[method](method);
+    }
+    return COLORS.Default(method);
+};
+
+//#endregion
+
+
+class SuperLogger {
+
+    // These are arbetrary values to make it possible for us to sort our logg messages. 
+    static LOGGING_LEVELS = {
+        ALL: 0,         // We output everything, no limits
+        VERBOSE: 5,     // We output a lott, but not 
+        NORMAL: 10,     // We output a moderate amount of messages
+        IMPORTANT: 100, // We output just siginfican messages
+        CRTICAL: 999    // We output only errors. 
+    };
+
+    // What level of messages should we be logging.
+    // This field is static 
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static
+    #globalThreshold = SuperLogger.LOGGING_LEVELS.ALL;
+
+    // Structure to keep the misc log functions that get created.
+    // This field is private.
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_properties
+    #loggers;
+
+    //#region Using a variation on the singelton pattern
+    // https://javascriptpatterns.vercel.app/patterns/design-patterns/singleton-pattern
+    // This field is static 
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static
+    static instance = null;
+
+    constructor() {
+        // This constructor will allways return a refrence to the first instance created. 
+        if (SuperLogger.instance == null) {
+            SuperLogger.instance = this;
+            this.#loggers = [];
+            this.#globalThreshold = SuperLogger.LOGGING_LEVELS.NORMAL;
+        }
+        return SuperLogger.instance;
+    }
+    //#endregion
+
+
+    // This is our automatic logger, it outputs at a "normal" level
+    // It is just a convinent wrapper around the more generic createLimitedRequestLogger function
+    createAutoHTTPRequestLogger() {
+        return this.createLimitedHTTPRequestLogger({ threshold: SuperLogger.LOGGING_LEVELS.NORMAL });
+    }
+
+    createLimitedHTTPRequestLogger(options) {
+
+        // if no level is threshold, we default to NORMAL.
+        // We are using the logical or (||) 
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
+        const threshold = options.threshold || SuperLogger.LOGGING_LEVELS.NORMAL;
+
+        // Returning an anonymous function that binds local scope.
+        return (req, res, next) => {
+
+            // If the threshold provided is less then the global threshold, we do not logg
+            if (this.#globalThreshold > threshold) {
+                return;
+            }
+
+            // Finaly we parse our request on to the method that is going to writ the log msg.
+            this.#LogHTTPRequest(req, res, next);
+        }
+
+    }
+
+    #LogHTTPRequest(req, res, next) {
+        // These are just some variables that we extract to show the point 
+        // TODO: Extract and format information important for your dev process. 
+        let type = req.method;
+        const path = req.originalUrl;
+        const when = new Date().toLocaleTimeString();
+
+        // TODO: This is just one simple thing to create structure and order. Can you do more?
+        type = colorize(type);
+        console.log(when, type, path);
+
+        // On to the next handler function
+        next();
+    }
+
 }
 
-class superLogger{
 
-static currentThresholdValue
-
-function log(req, res, next){
-
-const LOGGING_LEVELS = {
-    notImportant: 0,
-
-}
-
-
-let shouldLog = false;
-
-
-let type = req.method;
-const path = req.originalURL;
-const when = new Date().toLocaleTimeString();
-
-// if(type === HTTPMethods.POST){
-//     type = chalk.yellow(type);
-// }else if (type === HTTPMethods.GET)
-// type = chalk.green(type)
-// }
-
-type = colorize(type);}
-
-}
-
-next();
-
-export default superLogger;
+export default SuperLogger
--- routes/usersRoute.mjs
+++ routes/usersRoute.mjs
@@ -1,56 +1,62 @@
-import express from "express";
+import express, { response } from "express";
 import User from "../modules/user.mjs";
-import HttpCodes from "../modules/httpConstants.mjs";
+import { HTTPCodes, HTTPMethods } from "../modules/httpConstants.mjs";
+
 
 const USER_API = express.Router();
+USER_API.use(express.json); // This makes it so that express parses all incoming payloads as JSON for this route.
 
 const users = [];
 
-const helloWorldMiddleWare = function(req, res, next){
-  console.log("Hello WOrld");                                       //Her går den videre og vi kan ha nye brackets og kjøre ny kode.
-  next();
-}
 
-//user.USER_API.use(hello); Nå vil alle skrive hello.
+USER_API.get('/:id', (req, res, next) => {
 
-USER_API.get("/:id", (req, res, next) => {
-  // Return user object
+
+    // Tip: All the information you need to get the id part of the request can be found in the documentation 
+    // https://expressjs.com/en/guide/routing.html (Route parameters)
+
+    /// TODO: 
+    // Return user object
+})
+
+
+USER_API.post('/', (req, res, next) => {
+
+    // This is using javascript object destructuring.
+    // Recomend reading up https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#syntax
+    // https://www.freecodecamp.org/news/javascript-object-destructuring-spread-operator-rest-parameter/
+    const { name, email, password } = req.body;
+
+    if (name != "" && email != "" && password != "") {
+        const user = new User();
+        user.name = name;
+        user.email = email;
+
+        ///TODO: Do not save passwords.
+        user.pswHash = password;
+
+        ///TODO: Does the user exist?
+        let exists = false;
+
+        if (!exists) {
+            users.push(user);
+            res.status(HTTPCodes.SuccesfullRespons.Ok).end();
+        } else {
+            res.status(HTTPCodes.ClientSideErrorRespons.BadRequest).end();
+        }
+
+    } else {
+        res.status(HTTPCodes.ClientSideErrorRespons.BadRequest).send("Mangler data felt").end();
+    }
+
 });
 
-USER_API.post("/", (req, res, next) => {
-  // create user
+USER_API.put('/:id', (req, res) => {
+    /// TODO: Edit user
+})
 
-  const [name, email, password] =  [req.body.name, req.body.email, req.body.password]
-
-  if (name != "" && email != "" && password) {
-    const user = new User();
-    user.name = name;
-    user.email = email;
-    //Todo - Do not save password
-    user.pswHash = password;
-
-//Does the user exsist?
-
-let exists = false; //Må endres
-if("exists"){
-    users.push(user);
-}
-
-
-
-res.status(HttpCodes.succesfull.Ok);
-
-  } else {
-    res.status(HttpCodes.ClientSideErrorResponse.BadRequest).send("Missing parameters").end();
-  }
-});
-
-USER_API.put("/:id", (req, res) => {
-  // edit user
-});
-
-USER_API.delete("/:id", (req, res) => {
-  // create user
-});
+USER_API.delete('/:id', (req, res) => {
+    /// TODO: Delete user.
+})
 
 export default USER_API
